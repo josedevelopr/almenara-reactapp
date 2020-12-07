0ca2bf6069c983bcda9bd6fa26081b521a11feca
Aimar Andony <aimarandony10@gmail.com>
2020-12-06 22:14:16 -0500

    change url axios

--- src/config/clienteAxios.js
+++ src/config/clienteAxios.js
@@ -1,10 +1,10 @@
 import Axios from "axios";
 
-const LOCAL = "http://localhost:8080/";
-const CLOUD = "https://hospital-almenara-api.herokuapp.com/"
+// const LOCAL = "http://localhost:8080/";
+const URL_NUBE = "https://hospital-almenara-api.herokuapp.com/"
 
 const clienteAxios = Axios.create({
-  baseURL: CLOUD,
+  baseURL: URL_NUBE,
 });
 
 export default clienteAxios;
