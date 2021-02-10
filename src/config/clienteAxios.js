f764d2fb52e40ebfc4046d9a66bb46b3c8a2b4b3
Cristopher Armas <CristopherArmas23@gmail.com>
2021-02-10 18:14:08 -0500

    Commit, Jose on tas?

--- src/config/clienteAxios.js
+++ src/config/clienteAxios.js
@@ -2,6 +2,8 @@ import Axios from "axios";
 
  const LOCAL = "http://localhost:8080/";
  const URL_NUBE = "https://almenara-api.herokuapp.com/"
+ //const URL_NUBE = "https://sistemacontrolmedicoalmenara.com/hospital-almenara-api-0.0.1-SNAPSHOT/"
+ 
 
 const clienteAxios = Axios.create({
   baseURL: URL_NUBE,
